5af31bee4a7e13d502e15dfcdc19f7cf1084cb3c
Zeus - Home <blk.zeus@gmail.com>
2022-07-05 17:49:32 -0400

    Sistema Base - Creacion Proyecto

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+            <li>
+            <a href="#"">
+                {{ __('Creative Tim') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('About Us') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('Blog') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('Licenses') }}
+            </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> and <a href="https://www.updivision.com" target="_blank">UPDIVISION</a> for a better web.
+        </div>
+    </div>
+</footer>
